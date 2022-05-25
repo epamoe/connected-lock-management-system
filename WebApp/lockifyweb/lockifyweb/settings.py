bbf6cc8086c958245759a5f90435b97886e799dc
vignol <89270861+vignol@users.noreply.github.com>
2022-05-25 19:10:38 +0200

    login2

--- WebApp/lockifyweb/lockifyweb/settings.py
+++ WebApp/lockifyweb/lockifyweb/settings.py
@@ -134,4 +134,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL= "/"
+LOGIN_REDIRECT_URL= "index"
+LOGOUT_REDIRECT_URL = "/"
